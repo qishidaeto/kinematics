940f5f446fbaa9b8d0c1baa7f4c13db876acedab
qishidaeto <qishidaeto@gmail.com>
2021-04-04 22:32:05 +0300

    Shaders bufgix. Refactoring.

--- kinematics/main.cpp
+++ kinematics/main.cpp
@@ -63,14 +63,29 @@ struct coordinateSystem
 	GLuint VAO, VBO;
 };
 
-
 const GLuint screenWidth = 1366, screenHeight = 768;
 
 // Callback-functions
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 
 // Camera
 Camera camera(glm::vec3(10.0f, 10.0f, 10.0f));
+
+bool keys[1024];
+GLfloat lastX = screenWidth / 2.0f, lastY = screenHeight / 2.0f;
+bool firstMouse = true;
+
+GLfloat deltaTime = 0.0f;
+GLfloat lastFrame = 0.0f;
+
+void Do_Movement();
+
+// Transformation matrices
+glm::mat4 model(1.0f);
+glm::mat4 view(1.0f);
+glm::mat4 projection(1.0f);
 
 int main()
 {
@@ -84,7 +99,8 @@ int main()
 	glfwMakeContextCurrent(window);
 
 	glfwSetKeyCallback(window, key_callback);
-
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 
 	glewExperimental = GL_TRUE;
 	glewInit();
@@ -94,7 +110,7 @@ int main()
 	glViewport(0, 0, width, height);
 
 
-	Shader axesShader("fragment.shader", "vertex.shader");
+	Shader shader("shader.vs", "shader.frag");
 	coordinateSystem XYZ;
 
 	while (!glfwWindowShouldClose(window))
@@ -104,16 +120,9 @@ int main()
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glm::mat4 model;
-		glm::mat4 view;
-		glm::mat4 projection;
-
 		model = glm::mat4(1.0f);
 		view = camera.GetViewMatrix();
 		projection = glm::perspective(camera.Zoom, (float)screenWidth / (float)screenHeight, 0.01f, 1000.0f);
-
-
-
 
 		glfwSwapBuffers(window);
 	}
@@ -123,8 +132,51 @@ int main()
 	return 0;
 }
 
-	void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+void Do_Movement()
+{
+	// Camera controls
+	if (keys[GLFW_KEY_W])
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (keys[GLFW_KEY_S])
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (keys[GLFW_KEY_A])
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (keys[GLFW_KEY_D])
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	if (key >= 0 && key < 1024)
 	{
-		if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-			glfwSetWindowShouldClose(window, GL_TRUE);
+		if (action == GLFW_PRESS)
+			keys[key] = true;
+		else if (action == GLFW_RELEASE)
+			keys[key] = false;
 	}
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	GLfloat xoffset = xpos - lastX;
+	GLfloat yoffset = lastY - ypos;
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
+}
